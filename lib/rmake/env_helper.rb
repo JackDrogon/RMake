88f1ea7114731cc86bd7babd9880fa7d3e932431
Jack Drogon <jack.xsuperman@gmail.com>
2023-06-02 23:15:41 +0800

    Add doc for EnvHelper
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- lib/rmake/env_helper.rb
+++ lib/rmake/env_helper.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-# EnvHelper for some helper function moudle mixin for Env
+# EnvHelper is a module that provides helper functions for the Env class.
 module EnvHelper
+  # Executes the block if the verbose level is at least 1.
   def v
     yield if _verbose >= 1 && block_given?
   end
 
+  # Executes the block if the verbose level is at least 2.
   def vv
     yield if _verbose >= 2 && block_given?
   end
 
+  # Returns a string of spaces for indentation based on the ENV_INDENT value.
   def indent_space
     '  ' * @env[ENV_INDENT]
   end
 
+  # Increases the ENV_INDENT value, executes the block, and then decreases the ENV_INDENT value.
   def indent_monitor
     @env[ENV_INDENT] += 1
     yield if block_given?
@@ -22,6 +26,7 @@ module EnvHelper
 
   private
 
+  # Returns the verbose level as an integer.
   def _verbose
     @env.fetch('VERBOSE', 0).to_i
   end
